99a0e2ae43c4d6b1d463bf5ea4eeea85ccd8de34
Emma <hello@prpl.wtf>
2025-05-24 18:21:10 +0200

    feat: light/dark mode switching logic

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -2,5 +2,25 @@
   <div>
     <NuxtRouteAnnouncer />
     <p>blueprinting so hard i ran out of cyan ink</p>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
   </div>
 </template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+const updateTheme = () => {
+  useHead({
+    htmlAttrs: {
+      'data-bs-theme': colorMode.preference,
+    },
+  })
+}
+
+updateTheme()
+watch(colorMode, () => updateTheme())
+</script>
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -2,14 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon', '@nuxt/fonts'],
+  modules: ['@nuxt/image', '@nuxt/icon', '@nuxt/fonts', '@nuxtjs/color-mode'],
   css: ['halfmoon/css/halfmoon.min.css'],
   app: {
     head: {
       title: 'Blueprint',
       htmlAttrs: {
         lang: 'en',
-        'data-bs-theme': 'dark',
       },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1',

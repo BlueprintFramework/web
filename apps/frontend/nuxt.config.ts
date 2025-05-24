33bc10a51c24d988a387497c2a73fe4893e56d47
Emma <hello@prpl.wtf>
2025-05-24 21:39:13 +0200

    feat: switch to reka-ui with tailwindcss

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,26 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   srcDir: 'src/',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon', '@nuxt/fonts', '@nuxtjs/color-mode'],
-  css: [
-    'halfmoon/css/halfmoon.min.css',
-    'halfmoon/css/cores/halfmoon.modern.css',
-
-    '~/assets/css/utilities.css',
-  ],
-
+  modules: ['@nuxt/image', '@nuxt/icon', '@nuxt/fonts', 'reka-ui/nuxt'],
+  css: ['~/assets/css/app.css'],
   app: {
     head: {
       title: 'Blueprint',
       htmlAttrs: {
         lang: 'en',
-        'data-bs-theme': 'system',
-        'data-bs-core': 'modern',
       },
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
     },
   },
+
+  vite: { plugins: [tailwindcss()] },
 })
--- apps/frontend/src/plugins/useBootstrap.client.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.provide('bootstrap', bootstrap)
-})

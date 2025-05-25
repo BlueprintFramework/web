e93f4419b8412e6598764e1d23c57f33ecafbd25
Slimi1 <vslimi44@gmail.com>
2025-05-25 14:04:05 +0300

    feat: add responsiveness and move content in the middle

--- apps/frontend/src/app.vue
+++ apps/frontend/src/app.vue
@@ -1,13 +1,17 @@
 <template>
   <NuxtRouteAnnouncer />
-  <div>
+  <div class="min-h-screen flex flex-col bg-black text-white">
     <Navbar />
-    <BrandEmblem />
-    <h1>web-rewrite my beloved</h1>
-    <p>
-      blueprinting so hard i ran out of cyan ink
-      <br />
-      also this font is not it, go yell at emma
-    </p>
+    <main class="flex-grow container mx-auto flex flex-col items-center justify-center">
+      <div class="w-full max-w-4xl text-center">
+        <BrandEmblem class="mx-auto mb-6" />
+        <h1 class="font-bold mb-4">web-rewrite my beloved</h1>
+        <p class="mb-8">
+          blueprinting so hard i ran out of cyan ink
+          <br />
+          also this font is not it, go yell at emma
+        </p>
+      </div>
+    </main>
   </div>
 </template>

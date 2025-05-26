a1313a5920049669d1e24035a1a08d87607fde0a
Emma <hello@prpl.wtf>
2025-05-26 20:58:17 +0200

    feat: video?????

--- apps/frontend/src/components/ui/marketing/sections/Hero.vue
+++ apps/frontend/src/components/ui/marketing/sections/Hero.vue
@@ -1,34 +1,76 @@
 <template>
   <div
     :class="props?.class"
-    class="grid grid-cols-2 overflow-hidden rounded-3xl border border-neutral-700 bg-neutral-950"
+    class="grid grid-cols-2 grid-rows-2 overflow-hidden rounded-3xl border border-neutral-700 bg-neutral-950"
   >
     <div class="grid grid-rows-2">
-      <div class="space-y-3 self-center border-b border-neutral-700 p-16">
-        <Icon name="memory:anvil" mode="svg" :size="48" />
-        <h1>A solid platform for Pterodactyl® mods</h1>
-        <span class="opacity-75">
-          Pterodactyl modifications, addons and themes have suffered for ages.
-          Blueprint brings change, allowing Pterodactyl and it's modding
-          industry to reach new heights.
-        </span>
+      <div class="grid h-full border-b border-e border-neutral-700 p-16">
+        <div class="space-y-3 self-center">
+          <Icon name="memory:anvil" mode="svg" :size="48" />
+          <h1>A solid platform for Pterodactyl® mods</h1>
+          <span class="opacity-75">
+            Pterodactyl modifications, addons and themes have suffered for ages.
+            Blueprint brings change, allowing Pterodactyl and it's modding
+            industry to reach new heights.
+          </span>
+        </div>
       </div>
-      <div class="space-y-3 self-center p-16">
-        <Icon name="memory:heart" mode="svg" :size="48" />
-        <h1>Engineered for people, not profit</h1>
-        <span class="opacity-75">
-          Blueprint is fisically sponsored by a 501(c)(3) nonprofit foundation.
-          We're funded by donations, corporate sponsors and affiliate revenue.
-        </span>
+      <div class="grid h-full border-b border-e border-neutral-700 p-16">
+        <div class="space-y-3 self-center">
+          <Icon name="memory:heart" mode="svg" :size="48" />
+          <h1>Engineered for people, not profit</h1>
+          <span class="opacity-75">
+            Blueprint is fisically sponsored by a 501(c)(3) nonprofit
+            foundation. We're funded by donations, corporate sponsors and
+            affiliate revenue.
+          </span>
+        </div>
       </div>
     </div>
-    <div class="border-s border-neutral-700">
+    <div class="border-b border-neutral-700">
       <NuxtImg
         src="/img/pterodactyl.jpeg"
         :width="1280"
         :height="1024"
         class="aspect-video h-full w-full object-cover"
       />
+    </div>
+    <div class="border-e border-neutral-700">
+      <video
+        width="1280"
+        height="1024"
+        autoplay
+        muted
+        loop
+        class="h-full w-full object-cover"
+      >
+        <source src="/img/console.mp4" type="video/mp4" />
+        <source src="/img/console.webm" type="video/webm" />
+      </video>
+    </div>
+    <div class="grid grid-rows-2">
+      <div class="grid h-full border-b border-neutral-700 p-16">
+        <div class="space-y-3 self-center">
+          <Icon name="memory:terminal" mode="svg" :size="48" />
+          <h1>Powerful CLI utility</h1>
+          <span class="opacity-75">
+            Installs, removes and updates all of your extensions within minutes.
+            Save yourself from weekends of debugging, sleepless nights and
+            unreliable install scripts.
+          </span>
+        </div>
+      </div>
+      <div class="grid h-full p-16">
+        <div class="space-y-3 self-center">
+          <Icon name="memory:pickaxe" mode="svg" :size="48" />
+          <h1>Tools developers know and love</h1>
+          <span class="opacity-75">
+            We're constantly building tools to improve your developer
+            experience. Making Pterodactyl add-ons and themes is easier with
+            Blueprint.
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- apps/frontend/src/pages/index.vue
+++ apps/frontend/src/pages/index.vue
@@ -2,7 +2,7 @@
   <span class="md:display h1 mt-4 block xl:text-justify">
     <span class="text-gray-500">The</span>
     industry-leading modding platform
-    <span class="text-gray-500"> for the Pterodactyl® panel. </span>
+    <span class="text-gray-500"> for the Pterodactyl® panel </span>
   </span>
 
   <UiMarketingSectionsHighlights class="my-12" />

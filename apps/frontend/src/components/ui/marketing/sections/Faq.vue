234a7bf33bc64556ee1b77554fbcdb0c3ad14668
Emma <hello@prpl.wtf>
2025-05-29 20:31:22 +0200

    feat: work on FAQ.vue

--- apps/frontend/src/components/ui/marketing/sections/Faq.vue
+++ apps/frontend/src/components/ui/marketing/sections/Faq.vue
@@ -5,29 +5,44 @@
   >
     <div class="border-e border-neutral-700">
       <div
-        class="flex flex-row justify-between border-b border-neutral-700 p-4"
+        class="hover:text-brand-50 flex flex-row justify-between border-b border-neutral-700 p-4 transition-colors hover:bg-neutral-900"
       >
         <h2>What's Blueprint?</h2>
         <Icon name="memory:chevron-down" :size="24" />
       </div>
       <div
-        class="flex flex-row justify-between border-b border-neutral-700 p-4"
+        class="hover:text-brand-50 flex flex-row justify-between border-b border-neutral-700 p-4 transition-colors hover:bg-neutral-900"
       >
         <h2>Where can I discover and download extensions?</h2>
         <Icon name="memory:chevron-down" :size="24" />
       </div>
       <div
-        class="flex flex-row justify-between border-b border-neutral-700 p-4"
+        class="hover:text-brand-50 flex flex-row justify-between border-b border-neutral-700 p-4 transition-colors hover:bg-neutral-900"
       >
         <h2>Does Blueprint work with Pterodactyl forks?</h2>
         <Icon name="memory:chevron-down" :size="24" />
       </div>
-      <div class="flex flex-row justify-between p-4">
-        <h2>Are Blueprint's financials publicly available?</h2>
+      <div
+        class="hover:text-brand-50 flex flex-row justify-between p-4 transition-colors hover:bg-neutral-900"
+      >
+        <h2>What is Blueprint's financial structure?</h2>
         <Icon name="memory:chevron-down" :size="24" />
       </div>
     </div>
-    <div>test2</div>
+    <div class="relative overflow-hidden">
+      <NuxtImg
+        src="/img/gradient.jpeg"
+        :width="1280"
+        :height="300"
+        class="h-full w-full object-cover"
+      />
+      <Icon
+        name="memory:tooltip-above-help"
+        mode="svg"
+        :size="96"
+        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+      />
+    </div>
   </div>
 </template>
 

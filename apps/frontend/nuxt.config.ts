667798da3233a9f8c88c780a87c4ecd3a4fc675f
prplwtf <hello@prpl.wtf>
2025-06-01 22:51:34 +0200

    feat: comments begone

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,16 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   srcDir: 'src/',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/icon',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    'nuxt-marquee',
-  ],
+  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxt/image', 'nuxt-marquee'],
   css: ['~/assets/css/main.css'],
   app: {
     head: {

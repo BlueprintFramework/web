85d755901e1aefe0eeeddbd6be488dcc4887f66d
Emma <hello@prpl.wtf>
2025-06-02 17:23:45 +0200

    feat: improve navbar buttons (they should prolly be components though)

--- apps/frontend/src/components/ui/Navigation.vue
+++ apps/frontend/src/components/ui/Navigation.vue
@@ -11,19 +11,44 @@
           <BrandEmblem />
           <span class="text-xl font-bold">Blueprint</span>
         </NuxtLink>
-        <div class="flex items-center justify-between space-x-4">
-          <NuxtLink to="/browse" class="hover:text-brand-50 transition-colors">
+        <div
+          class="flex flex-row items-center justify-between space-x-3 text-sm"
+        >
+          <NuxtLink
+            to="/browse"
+            class="hover:text-brand-50 rounded-md border border-neutral-700 bg-neutral-900 px-2.5 py-0.5 transition-colors hover:bg-neutral-800"
+          >
             Extensions
           </NuxtLink>
-          <NuxtLink to="/docs" class="hover:text-brand-50 transition-colors">
+          <NuxtLink
+            to="/docs"
+            class="hover:text-brand-50 rounded-md border border-neutral-700 bg-neutral-900 px-2.5 py-0.5 transition-colors hover:bg-neutral-800"
+          >
             Documentation
           </NuxtLink>
+          <div
+            class="flex flex-row items-center divide-x divide-neutral-700 overflow-hidden rounded-md border border-neutral-700 bg-neutral-800"
+          >
+            <NuxtLink
+              to="/auth"
+              class="hover:text-brand-50 bg-neutral-900 px-2.5 py-0.5 transition-colors hover:bg-neutral-800"
+            >
+              <span>Log in</span>
+            </NuxtLink>
+            <NuxtLink
+              to="/auth?register=true"
+              class="hover:text-brand-50 flex flex-row items-center gap-1 px-2.5 py-0.5 transition-colors"
+            >
+              <span>Sign up</span>
+              <Icon name="memory:chevron-right" />
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
     <div
       class="bg-linear-to-r h-[1px] w-full from-neutral-800 via-neutral-500 to-neutral-800"
-    ></div>
+    />
   </nav>
   <div class="h-13"></div>
 </template>

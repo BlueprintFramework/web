1c799ff7f8327afe4a0d8b6fe38f167b5133095d
prplwtf <hello@prpl.wtf>
2025-06-04 13:36:07 +0200

    feat: minor shenanigans

--- apps/frontend/src/pages/index.vue
+++ apps/frontend/src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="space-y-12">
-    <span class="md:display h1 mt-4 block xl:text-justify">
+  <div class="space-y-12 pt-4">
+    <span class="md:display h1 block xl:text-justify">
       <span class="text-gray-500">The</span>
       industry-leading modding platform
       <span class="text-gray-500"> for the Pterodactyl® panel </span>

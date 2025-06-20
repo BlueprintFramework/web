94ac818523219c387ba1dbf85d8c2ffe2856a73b
Emma <hello@prpl.wtf>
2025-06-20 21:47:00 +0200

    feat: add nuxt content

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -4,7 +4,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   srcDir: 'src/',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxt/image', 'nuxt-marquee'],
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'nuxt-marquee',
+    '@nuxt/content',
+  ],
   css: ['~/assets/css/main.css'],
   app: {
     head: {

6a68c6dfa84b8133472f150d16b946903c7c6c4f
Emma <hello@prpl.wtf>
2025-06-21 12:18:39 +0200

    feat: academy????

--- apps/frontend/src/components/ui/Navigation.vue
+++ apps/frontend/src/components/ui/Navigation.vue
@@ -19,6 +19,12 @@
             Extensions
           </NuxtLink>
           <NuxtLink
+            to="/academy"
+            class="hover:text-brand-50 rounded-md border border-neutral-700 bg-neutral-900 px-2.5 py-0.5 transition-colors hover:bg-neutral-800"
+          >
+            Academy
+          </NuxtLink>
+          <NuxtLink
             to="/docs"
             class="hover:text-brand-50 rounded-md border border-neutral-700 bg-neutral-900 px-2.5 py-0.5 transition-colors hover:bg-neutral-800"
           >
---
+++ apps/frontend/src/pages/academy.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>/academy</h1>
+</template>

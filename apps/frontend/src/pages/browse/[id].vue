4c1532ff7ddefad05341b956efdd10fcba13bdb8
Emma <hello@prpl.wtf>
2025-07-17 13:22:58 +0200

    feat: keep trying

--- apps/frontend/src/pages/browse/[id].vue
+++ apps/frontend/src/pages/browse/[id].vue
@@ -1,24 +1,15 @@
 <template>
   <div v-if="extension">
-    <div class="flex gap-4">
-      <div class="w-full">
-        <div
-          class="w-full overflow-hidden rounded-3xl border border-neutral-700"
-        >
-          <div class="p-4">
-            <NuxtImg
-              :src="`https://s3.blueprint.zip/extensions/${extension.identifier}.jpeg`"
-              class="aspect-video w-full rounded-2xl border border-neutral-700 object-cover"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="min-w-100">
-        <div
-          class="w-full divide-y divide-neutral-700 rounded-3xl border border-neutral-700"
-        >
-          <div class="space-y-2 p-4">
+    <div
+      class="overflow-hidden rounded-3xl border border-neutral-700 bg-neutral-950"
+    >
+      <div class="flex divide-x divide-neutral-700">
+        <div class="w-full">
+          <NuxtImg
+            :src="`https://s3.blueprint.zip/extensions/${extension.identifier}.jpeg`"
+            class="h-100 w-full border-b border-neutral-700 object-cover"
+          />
+          <div class="space-y-3 p-8">
             <h1 class="truncate">
               {{ extension.name }}
             </h1>
@@ -30,7 +21,26 @@
               {{ extension.author.name }}
             </p>
           </div>
-          <div class="p-4">wrrf</div>
+        </div>
+        <div class="min-w-100">
+          <button
+            type="submit"
+            class="text-default-font hover:text-brand-50 flex w-full cursor-pointer items-center justify-between border-b border-neutral-700 bg-neutral-950 px-4 py-3 transition-colors hover:bg-neutral-900"
+          >
+            <div class="text-xl font-semibold">
+              <span>Buy on BuiltByBit</span>
+            </div>
+            <Icon name="memory:chevron-right" mode="svg" :size="24" />
+          </button>
+          <button
+            type="submit"
+            class="text-default-font hover:text-brand-50 flex w-full cursor-pointer items-center justify-between border-b border-neutral-700 bg-neutral-950 px-4 py-3 transition-colors hover:bg-neutral-900"
+          >
+            <div class="text-xl font-semibold">
+              <span>Buy on sourceXchange</span>
+            </div>
+            <Icon name="memory:chevron-right" mode="svg" :size="24" />
+          </button>
         </div>
       </div>
     </div>

aac2170dce7dce41e8b0649a149e0f3482be7ddb
Emma <hello@prpl.wtf>
2025-07-28 13:51:56 +0200

    feat: wip /app

--- apps/frontend/src/pages/app/index.vue
+++ apps/frontend/src/pages/app/index.vue
@@ -4,17 +4,34 @@
   <div
     class="grid grid-cols-4 divide-x divide-neutral-700 rounded-3xl border border-neutral-700"
   >
-    <div class="p-4">
+    <div class="space-y-2 p-4">
       <div class="flex items-center gap-1.5">
         <Icon name="memory:account-box" :size="28" mode="svg" />
-        <span class="text-xl font-bold">Account settings</span>
+        <span class="text-xl font-bold">Account</span>
       </div>
+      <span class="opacity-75">
+        Display name, password, email address, connections, 2-factor
+        authentication
+      </span>
     </div>
-    <div class="p-4">
+    <div class="space-y-2 p-4">
       <div class="flex items-center gap-1.5">
         <Icon name="memory:cube" :size="28" mode="svg" />
         <span class="text-xl font-bold">Extensions</span>
       </div>
+      <span class="opacity-75">
+        Create product, configure products, unlist products
+      </span>
     </div>
+    <div class="space-y-2 p-4">
+      <div class="flex items-center gap-1.5">
+        <Icon name="memory:chart-bar" :size="28" mode="svg" />
+        <span class="text-xl font-bold">Statistics</span>
+      </div>
+      <span class="opacity-75">
+        View statistics, customize graphs, embed widgets
+      </span>
+    </div>
+    <div class="bg-stripes" />
   </div>
 </template>

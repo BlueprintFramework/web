b39994b80516f2908a6f0c21e57b419a519029c8
Emma <hello@prpl.wtf>
2025-09-21 18:28:21 +0200

    feat: turn blurredcredential into a component

---
+++ apps/frontend/src/components/elements/Blurredcredential.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="rounded-2xl border border-neutral-700 p-2">
+    <div class="group me-2 rounded-2xl font-mono">
+      <span
+        class="wrap-break-word blur-sm transition-all group-hover:blur-none"
+      >
+        {{ props.content }}
+      </span>
+    </div>
+    <span
+      class="text-default-font/50 float-end me-2 bg-neutral-950 px-1 text-xs"
+    >
+      (hover to reveal)
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  content: string
+}>()
+</script>
--- apps/frontend/src/components/ui/app/user/Password.vue
+++ apps/frontend/src/components/ui/app/user/Password.vue
@@ -99,20 +99,7 @@
           Are you sure that you want to change your password?
         </div>
 
-        <div class="rounded-2xl border border-neutral-700 p-2">
-          <div class="group me-2 rounded-2xl font-mono">
-            <span
-              class="wrap-break-word blur-sm transition-all group-hover:blur-none"
-            >
-              {{ passwordForm.new_password }}
-            </span>
-          </div>
-          <span
-            class="text-default-font/50 float-end me-2 bg-neutral-950 px-1 text-xs"
-          >
-            (hover to reveal)
-          </span>
-        </div>
+        <ElementsBlurredcredential :content="passwordForm.new_password" />
 
         <template #footer>
           <ElementsButton

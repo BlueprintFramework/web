bc46372d9d7611e15415a4f6fc7dd9a92d5edcd6
Emma <hello@prpl.wtf>
2025-11-03 22:53:15 +0100

    feat: more footer work

--- apps/frontend/src/components/ui/footer/index.vue
+++ apps/frontend/src/components/ui/footer/index.vue
@@ -1,24 +1,65 @@
 <template>
   <div class="flex divide-x divide-neutral-700">
-    <div class="w-[500px] p-6">
-      <div
-        class="relative h-20 overflow-hidden rounded-2xl border border-neutral-700"
+    <div class="w-[500px] space-y-6 p-6">
+      <NuxtLink
+        to="/guides/admin/install"
+        class="group relative block h-20 w-full cursor-pointer overflow-hidden rounded-2xl border border-neutral-700 outline-0 transition-colors hover:border-neutral-500 focus:border-neutral-500"
+        @mousedown.prevent
+      >
+        <NuxtImg
+          src="/img/bluething.jpeg"
+          :width="200"
+          :height="80"
+          class="h-full w-full object-cover transition-all group-hover:scale-105 group-hover:opacity-75 group-focus:scale-105 group-focus:opacity-75"
+        />
+        <div
+          class="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform"
+        >
+          <div class="flex flex-col items-center">
+            <div class="flex items-center gap-1">
+              <span
+                class="group-hover:text-brand-50 group-focus:text-brand-50 text-xl font-bold transition-colors"
+                >Install Blueprint</span
+              >
+              <Icon
+                name="memory:chevron-right"
+                class="group-hover:text-brand-50 group-focus:text-brand-50 transition-colors"
+                :size="24"
+              />
+            </div>
+          </div>
+        </div>
+      </NuxtLink>
+
+      <NuxtLink
+        to="/releases"
+        class="group relative block h-20 w-full cursor-pointer overflow-hidden rounded-2xl border border-neutral-700 outline-0 transition-colors hover:border-neutral-500 focus:border-neutral-500"
+        @mousedown.prevent
       >
         <NuxtImg
           src="/img/releases/latest.png"
           :width="200"
           :height="80"
-          class="h-full w-full object-cover"
+          class="h-full w-full object-cover transition-all group-hover:scale-105 group-hover:opacity-75 group-focus:scale-105 group-focus:opacity-75"
         />
         <div
-          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+          class="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform"
         >
-          <div class="flex items-center gap-1">
-            <span class="text-xl font-bold">Release notes</span>
-            <Icon name="memory:chevron-right" />
+          <div class="flex flex-col items-center">
+            <div class="flex items-center gap-1">
+              <span
+                class="group-hover:text-brand-50 group-focus:text-brand-50 text-xl font-bold transition-colors"
+                >Release notes</span
+              >
+              <Icon
+                name="memory:chevron-right"
+                class="group-hover:text-brand-50 group-focus:text-brand-50 transition-colors"
+                :size="24"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </NuxtLink>
     </div>
     <div class="grid w-full grid-cols-3 gap-4 p-6">
       <div class="flex flex-col gap-1 text-neutral-400 decoration-transparent">

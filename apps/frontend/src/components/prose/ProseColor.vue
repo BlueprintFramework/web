f470d9ae4f68f36f893d581b15830fb07adbc471
Emma <hello@prpl.wtf>
2025-11-26 18:39:25 +0100

    feat: unoops docs

---
+++ apps/frontend/src/components/prose/ProseColor.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>hi</p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name?: string
+  rgb: string
+
+  // Disable class and style props
+  class?: string
+  style?: string
+}>()
+</script>
